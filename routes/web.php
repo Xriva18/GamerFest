684eb9bef6531fd230419b1f43c1af35b5f33820
nayarbP <pallangoruben2021@gmail.com>
2024-12-18 13:24:17 -0500

    dashboar incripciones

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::prefix('inscripciones-panel')
+    ->middleware(['auth'])
+    ->group(base_path('routes/inscripciones_panel.php'));
 require __DIR__.'/auth.php';
