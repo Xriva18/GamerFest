2997d1a692399a1b97976dee534c8d67f945a472
Xriva18 <kevinguanoluisa2004@gmail.com>
2024-12-27 09:19:35 -0500

    Agrega nuevos paneles para Coordinador, Participante y Tesorero en el sistema Filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CoordinadorPanelProvider::class,
+    App\Providers\Filament\ParticipantePanelProvider::class,
+    App\Providers\Filament\TesoreroPanelProvider::class,
 ];
