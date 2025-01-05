ab7ff61f7d39086e8a10e24c0c2bf708663df526
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-01-04 20:20:14 -0500

    mejorar el flujo de autenticación: deshabilitar ruta de dashboard y agregar vista de bienvenida

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,19 @@
+{{-- resources/views/bienvenido.blade.php --}}
+@extends('layouts.main') <!-- Usa el layout base -->
+
+@section('title', 'Bienvenido') <!-- Cambia el título para esta vista -->
+
+@section('content') <!-- Define el contenido que irá en la sección content -->
+    <x-guest-layout>
+        <div class="max-w-md mx-auto shadow-md rounded-lg p-6">
+            <h2 class="text-2xl font-semibold text-center mb-4">¡Bienvenido!</h2>
+            <p class="text-center mb-6">Ya estás inscrito a <strong>Gamer Fest 2025</strong>.</p>
+
+            <div class="flex justify-center">
+                <x-primary-button onclick="window.location.href='{{ route('login') }}'">
+                    {{ __('Login') }}
+                </x-primary-button>
+            </div>
+        </div>
+    </x-guest-layout>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,35 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UniversidadController;
 use App\Http\Controllers\FileUploadController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+
+/*Route::get('/', function (Request $request) {
+    // Cerrar la sesión del usuario
+    Auth::logout();
+
+    // Invalidar la sesión actual
+    $request->session()->invalidate();
+
+    // Regenerar el token de la sesión para prevenir ataques de tipo CSRF
+    $request->session()->regenerateToken();
+
+    // Retornar la vista de bienvenida
+    return view('welcome');
+});*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
+Route::get('/bienvenido', function () {
+    return view('bienvenido');
+})->name('bienvenido');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
